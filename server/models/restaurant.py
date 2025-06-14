b8096f12fcd3778df709b4d56cfda0de0b20b6a6
dennis.muchiri1@student.moringaschool.com <muchirid935@gmail.com>
2025-06-14 10:32:52 +0300

    done the restaurant.py

---
+++ server/models/restaurant.py
@@ -0,0 +1,11 @@
+from ..app import db
+
+class Restaurant(db.model):
+    __tablename__='restaurants'
+    id = db.column(db.integer,primary_key=True)
+    name = db.column(db.string,nullable=False)
+    address = db.column(db.string,nullable=False)
+
+    restaurant_pizzas = db.realationship('RestaurantPizzas', back_populates='restaurant', cascade='all,delete-orphan')
+     def __repr__(self):
+    return f'<Restaurant {self.name}>'
